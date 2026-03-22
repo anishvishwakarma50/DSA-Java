fa4469680b41e80ff140dc56278f2b8d96cab7c4
anishvishwakarma50 <vishwakarmaanish50@gmail.com>
2026-03-22 23:28:11 +0530

    Find Square root of Number using Binary Search

---
+++ 12_Find_Sqrt_Of_Number_using_binary_search/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    static int sqrt(int n) {
+
+        int start = 0;
+        int end = n;
+        int result = 0;
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (mid * mid == n) {
+                return mid;
+            }
+
+            else if (mid * mid < n) {
+                result = mid;       // store possible answer
+                start = mid + 1;    // try bigger
+            }
+
+            else {
+                end = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sqrt(16)); // 4
+        System.out.println(sqrt(20)); // 4
+    }
+}
