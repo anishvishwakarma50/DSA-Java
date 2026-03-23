6e5ea61425e9e3de425c4debceec99f5ad714142
anishvishwakarma50 <vishwakarmaanish50@gmail.com>
2026-03-23 23:48:58 +0530

    Find Smallest Number whose Square is >= Target

---
+++ 13_Find_smallest_number_square/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    static int sqrt(int n) {
+
+        int start = 0;
+        int end = n;
+        int result = 0;
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (mid * mid == n) {
+                return mid;
+            }
+
+            else if (mid * mid >= n) {
+                result = mid;       // store possible answer
+                end = mid - 1;    // try smaller
+            }
+
+            else {
+                start = mid + 1;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sqrt(20));
+    }
+}
